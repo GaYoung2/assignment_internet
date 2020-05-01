49f86e9eead0393ae110b9a264a3fdab319a3f88
Kim Ga Young <bunny0098@naver.com>
2020-05-01 21:35:12 +0900

    메인문 몬스터와 커비움직이기

--- main_monster.py
+++ main_monster.py
@@ -3,7 +3,7 @@ import sys
 import random
 from time import sleep
 
-padWidth = 480
+padWidth = 800
 padHeight = 640
 monsterimage = ["images/rock01.png","images/rock02.png"]
 white = (255, 255, 255)
@@ -17,10 +17,30 @@ def initGame():
     pygame.init()
     gamePad = pygame.display.set_mode((padWidth,padHeight))
     pygame.display.set_caption('PyShooting')
-    background =pygame.image.load("images/background.png").convert_alpha()
+    background =pygame.image.load("images/karbybackground2.png").convert_alpha()
     curby = pygame.image.load("images/fighter.png").convert_alpha()
     clock = pygame.time.Clock()
 
+def writeMessage(text):
+    global gamePad,gameoverSound
+    textfont = pygame.font.Font('NanumGothic.ttf',80)
+    text = textfont.render(text,True,(255,0,0))
+    textpos = text.get_rect()
+    textpos.center = (padWidth/2,padHeight/2)
+    pamePad.blit(text,textpos)
+    pygame.display.update()
+    pygame.mixer.music.stop()
+    gameoverSound.play()
+    sleep(2)
+    pygame.mixer.music.play(-1)
+    runGame()
+
+def crash():
+    global gamepad
+    writeMessage('충돌')
+def gameOver():
+    global gamePad
+    writeMessage('게임오버')
 
 def runGame():
     global gamePad,clock,background,curby
@@ -52,22 +72,28 @@ def runGame():
                 sys.exit()
             
             key_event = pygame.key.get_pressed()
-
-            if key_event[pygame.K_SPACE]:
-                curbyusey -= 1
-            #잠시 시간이 필요
-            #pos_y += 10
-            if key_event[pygame.K_LEFT]:
-                curbyusex -= 5
-
-            if key_event[pygame.K_RIGHT]:
-                curbyusex += 5
-
-            if key_event[pygame.K_UP]:
-                curbyusey -= 5
-
-            if key_event[pygame.K_DOWN]:
-                curbyusey += 5
+            if event.type == pygame.KEYDOWN:
+                if key_event[pygame.K_SPACE]:
+                    curbyusey -= 1
+                elif key_event[pygame.K_LEFT]:
+                    curbyusex -= 5
+                elif key_event[pygame.K_RIGHT]:
+                    curbyusex += 5
+                elif key_event[pygame.K_UP]:
+                    curbyusey -= 5
+                elif key_event[pygame.K_DOWN]:
+                    curbyusey += 5
+            elif event.type == pygame.KEYUP:
+                if key_event[pygame.K_SPACE]:
+                    curbyusey -= 0
+                elif key_event[pygame.K_LEFT]:
+                    curbyusex -= 0
+                elif key_event[pygame.K_RIGHT]:
+                    curbyusex += 0
+                elif key_event[pygame.K_UP]:
+                    curbyusey -= 0
+                elif key_event[pygame.K_DOWN]:
+                    curbyusey += 0
             #pygame.draw.circle(background, white, (pos_x, pos_y), 20)
 
         drawObject(background,0,0)
@@ -80,6 +106,8 @@ def runGame():
             curbyx = padWidth - curbyWidth
         drawObject(curby,curbyx,curbyy)
             #pygame.draw.circle(background, white, (pos_x, pos_y), 20)
+
+        if 
 
         monsterX -=monsterSpeed
         if monsterX < 0: #몬스터가 없어지면 새로운 몬스터를 부른다.
