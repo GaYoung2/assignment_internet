edd1a08a9ee25ac36038c58fff573b9fee34951d
Kim Ga Young <bunny0098@naver.com>
2020-05-02 02:40:42 +0900

    jump추가

--- moving_curby.py
+++ moving_curby.py
@@ -1,52 +1,116 @@
 import pygame
 import sys
+import random
+from time import sleep
+from pygame.locals import *
+pygame.font.init()
 
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
+padWidth = 500
+padHeight = 500
+monsterimage = ["images/rock01.png","images/rock02.png"]
+GRAY =(100,100,100)
+background_width = 500
 
-white = (255, 255, 255)
-black = (0, 0, 0)
+def drawObject(obj,x,y):
+    global gamePad
+    gamePad.blit(obj,(x,y))
 
-pygame.init() 
-pygame.display.set_caption("Simple PyGame Example")  #게임 화면 이름
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #게임스크린만들기
+def back(background,x,y):
+    global gamePad
+    gamePad.blit(background,(x,y))
+
+def initGame():
+    global gamePad,clock,background,curby,background2
+    pygame.init()
+    gamePad = pygame.display.set_mode((padWidth,padHeight))
+    pygame.display.set_caption('PyShooting')
+    background =pygame.image.load("images/karbybackground2.png").convert_alpha()
+    background2 = background.copy()
+    curby = pygame.image.load("images/fighter.png").convert_alpha()
+    clock = pygame.time.Clock()
 
 
-#커비 움직이기-----------------------------------------------------
-pos_x = 0
-pos_y = 400
+def runGame():
+    global gamePad,clock,background,curby,background2
+    background_x = 0
+    background2_x = background_width
+    isJump = False
+    jumpCount = 10
 
-clock = pygame.time.Clock() #시간설정
-while True:
-    clock.tick(120)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+    curbySize = curby.get_rect().size
+    curbyWidth = curbySize[0]
+    curbyHeight = curbySize[1]
+    curbyusex = 50
+    curbyusey = 440
+    vel = 5
 
-    key_event = pygame.key.get_pressed()
-    if key_event[pygame.K_SPACE]:
-        pos_y -= 10
-        #잠시 시간이 필요
-        #pos_y += 10
+    monster = pygame.image.load(random.choice(monsterimage))
+    monsterSize = monster.get_rect().size #monster 크기
+    monsterWidth = monsterSize[0]
+    monsterHeight = monsterSize[1]
+    #monster초기 위치 설정
+    monsterX = 440
+    monsterY = 440
+    monsterSpeed = 6
 
-    if key_event[pygame.K_LEFT]:
-        pos_x -= 1
+    crashed = False
+    #key_event = pygame.key.get_pressed()
+    while not crashed:
+        for event in pygame.event.get():
+            if event.type in [pygame.QUIT]:
+                pygame.quit()
+                sys.exit()
 
-    if key_event[pygame.K_RIGHT]:
-        pos_x += 1
+        key_event = pygame.key.get_pressed()
+            #if event.type == pygame.KEYDOWN:
+        if key_event[pygame.K_LEFT] and curbyusex >vel:
+            curbyusex -= vel
+        if key_event[pygame.K_RIGHT] and curbyusex < 500 - curbyWidth - vel:
+            curbyusex += vel
+        if not (isJump):
+            if key_event[pygame.K_UP] and curbyusey > vel:
+                curbyusey -= vel
+            if key_event[pygame.K_DOWN] and curbyusey < 500 - curbyHeight - vel:
+                curbyusey += vel
+            if key_event[pygame.K_SPACE]:
+                isJump = True
+        else:
+            if jumpCount >=-10:
+                neg = 1
+                if jumpCount <0:
+                    neg = -1
+                curbyusey -=(jumpCount**2)*0.5*neg
+                jumpCount -=1
+            else:
+                isJump = False
+                jumpCount = 10
+        drawObject(background,0,0)
+           
 
-    if key_event[pygame.K_UP]:
-        pos_y -= 1
+        background2_x-=2
+        background_x-=2
+        if background_x==-background_width:
+            background_x=background_width
+        if background2_x==-background_width:
+            background2_x=background_width
+        back(background,background_x,0)
+        back(background2,background2_x,0)
+        drawObject(curby,curbyusex,curbyusey)
 
-    if key_event[pygame.K_DOWN]:
-        pos_y += 1
+        monsterX -=monsterSpeed
+        if monsterX < 50: #몬스터가 없어지면 새로운 몬스터를 부른다.
+            monster = pygame.image.load(random.choice(monsterimage))
+            monsterSize = monster.get_rect().size
+            monsterWidth = monsterSize[0]
+            monsterHeight = monsterSize[1]
+            monsterX = 450
+            monsterY = 450
+        drawObject(monster,monsterX,monsterY)
+        pygame.display.update()
+        clock.tick(30)
+    pygame.quit()
 
-    screen.fill(black)
-    pygame.draw.circle(screen, white, (pos_x, pos_y), 20) #스크린에 흰색 크기가 20이고 x,y 좌표를가진것을 생성
-    pygame.display.update()
-#--------------------------------------------------------------------------
-
-#--------------------------------------------------------------------------
-#몬스터 만들어서 랜덤으로 배치하기
-
+initGame()
+runGame()
+#pygame.font.get_fonts()
 
